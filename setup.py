06690f89bc92c7a0dcc66100a477706fcbe13132
Cindy* <adamashton11@gmail.com>
2019-01-19 14:59:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-from distribute_setup import use_setuptools
-use_setuptools()
 
 #python setup.py sdist upload
 
@@ -14,7 +12,6 @@ setup(name='stratum',
       author_email='info@bitcion.cz',
       url='http://blog.bitcoin.cz/stratum',
       packages=['stratum',],
-      py_modules=['distribute_setup',],
       zip_safe=False,
       install_requires=['twisted', 'ecdsa', 'autobahn',]
      )
